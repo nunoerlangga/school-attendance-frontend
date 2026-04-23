8227b8f2f6301641b9712b9367778c4043a52a03
RizkiMuhammad-JMD <just.mydesign@gmail.com>
2026-04-23 09:30:59 +0700

    membenarkan bug 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/Login';
 import AdminDashboard from './components/AdminDashboard';
 import GuruDashboard from './components/GuruDashboard';
@@ -74,6 +74,7 @@ const App: React.FC = () => {
         <Route path="/" element={<Navigate to="/login" replace />} />
       </Routes>
     </Router>
+
   );
 };
 
